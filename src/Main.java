5dc6748aaf5eda0dd2ee849921d9bd31643e7da1
causicb <benjo1306@gmail.com>
2025-03-17 19:06:14 +0100

    Armstrong numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
 
     public static void main(String[] args) {
+        for (int i = 1; i < 10000; i++) {
+            int sum = 0;
+            int currentNumber = i;
+            int totalDigits = 0;
+
+            int counter = i;
+            while (counter > 0) {
+                counter /= 10;
+                totalDigits++;
+            }
+
+            while (currentNumber != 0) {
+                int oneDigit = currentNumber % 10;
+                sum += (int) Math.pow(oneDigit, totalDigits);
+                currentNumber /= 10;
+            }
+
+            if (i == sum) {
+                System.out.println("Armstrong number: " + i);
+            }
+
+        }
 
     }
 
